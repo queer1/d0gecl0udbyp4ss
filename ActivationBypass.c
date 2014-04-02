4e2df9ff51ba2a6232f68396252b5582a0660af0
d0gecl0ud <raindoge@gmail.com>
2014-04-01 20:35:45 -0500

    Create ActivationBypass.c

---
+++ ActivationBypass.c
@@ -0,0 +1,37 @@
+/* Thanks for everything Gadgetwide u is da best
+*Copyright YouDontEvenKnowC Corportation
+*Built 2014
+*we are #internal of #appl
+*/
+
+if {
+	allocated_memory = 128k return kernel val 0x0000000000 bash run limera1n
+	call @geohot
+		pull time _requestingAssistanceGeoHot
+		messageID= ["Pls Activates me"\n"]
+	call malloc @tomcr00se pwned _Saved_ iCloud
+	call com.apple.icloud 
+	
+	return killall -9 backboardd SpringBoard
+	onRespring boot _asPwned
+catch _ThisIsntReal
+	call @dogera1n pID pwned_ maclloc
+		
+		system.out.print
+		
+	run _r0bf0rd_
+			{ send 0 bytes }
+	
+	@override _iCLoudIsNotActivated 
+		_saveState
+	@override _AppleIsGud
+		_thisisfake
+	
+try
+	_iCloudActivation
+		if {
+			iCLoudAvationIsOn= true
+			}
+			
+}
+		
