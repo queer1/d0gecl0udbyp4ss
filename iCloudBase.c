79a5b0735b8ea0b0e325e4e955c9142b4efffc35
d0gecl0ud <raindoge@gmail.com>
2014-04-01 20:36:59 -0500

    Create iCloudBase.c

---
+++ iCloudBase.c
@@ -0,0 +1,17 @@
+/*Copyright 2014
+*Built April 1, 2014
+*/
+
+
+#include <stdio.h>
+#include <r0bf0rd.h>
+#include <aprlflsx.h>
+#include <iCLoudBase.h>
+
+if {
+	_iCloudLocked_=true set void 0x0000 malloc 
+	for malloc_runCloud set val=false x int = void
+else
+	_RunPwnedR0bF0rd_=false
+return void
+}
