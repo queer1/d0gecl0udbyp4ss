ae7f379d6c9fa6c8d29ae4221f48403cea295cb9
d0gecl0ud <raindoge@gmail.com>
2014-04-01 20:38:03 -0500

    Create r0bf0rd.c

---
+++ r0bf0rd.c
@@ -0,0 +1,21 @@
+/* Copyright 2014 VaroFans LLC
+*Thanks to Gadgetwide and their 25k employees
+*Also thanks to iH8sn0w for being awesome and making a real iCLoud bypass tool
+*/
+
+#include <stdio.h>
+#include <r0bf0rd.h>
+
+if {
+	_iCLoudActivationSeemsLegit_ return 0x0000 kernel fault malloc _blah_
+	for _PwnediCloudState
+try 
+	fetch https://secure.r0bf0rdsn0w.com/activatethis return value null 0x00 nullpoint **0 *goto
+	fail;;gotofail
+	
+catch _this run malloc _is 0x000 if _fake 
+	{ you_is_stupid_
+	
+	}
+	
+}
